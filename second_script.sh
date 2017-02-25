8db189e69688d2ce4f48fb1e6ab6cfdfcd05f5ea
John E. Smith III <drjes3@gmail.com>
2017-02-25 12:18:44 -0700

    updated with help

--- second_script.sh
+++ second_script.sh
@@ -1,3 +1,8 @@
+#this is used to count the number of characters in a file
+if [$1 == "-h"] ||[$1 == "--help"]; then
+	echo "Usage: sh $0 input_file output file"
+	exit
+fi
 head -5 $1 |sort > temp.txt
 wc -c temp.txt > $2
 rm temp.txt
